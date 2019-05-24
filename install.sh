0a2c295c305d28c009d94989a5d61f03d0e6b4e5
graylagx2 <50938347+graylagx2@users.noreply.github.com>
2019-05-24 15:32:10 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-[[ -f apkbleach ]] && chmod a+x BypassAV && mv apkbleach /usr/local/bin 
+[[ -f apkbleach ]] && chmod a+x apkbleach && mv apkbleach /usr/local/bin 
 [[ -x /usr/local/bin/apkbleach ]] && rm $0 && exit
 
